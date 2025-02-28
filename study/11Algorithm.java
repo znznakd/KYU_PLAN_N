e264a0d3b2f9943232edcc1c6942575fb5dfe034
znznakd <64431656+znznakd@users.noreply.github.com>
2025-02-28 23:06:09 +0900

    Update 11Algorithm.java

--- study/11Algorithm.java
+++ study/11Algorithm.java
@@ -1,1 +1,29 @@
-TEST
+public int maxDona(int[] donations) {
+    
+    int max = 0;
+    int[] dpFi = new int[donations.length];
+    
+    for (int i = 0; i < donations.length-1; i++) {
+        dpFi[i] = donations[i];
+        if (i >= 3) {
+            dpFi[i] += dpFi[i-3] > dpFi[i-2] ? dpFi[i-3] : dpFi[i-2];
+        } else if (i >= 2) {
+            dpFi[i] += dpFi[i-2];
+        }
+        if (max < dpFi[i]) max = dpFi[i];
+    }
+    
+    int[] dpSe = new int[donations.length];
+    
+    for (int i = 1; i < donations.length; i++) {
+        dpSe[i] = donations[i];
+        if (i >= 3) {
+            dpSe[i] += dpSe[i-3] > dpSe[i-2] ? dpSe[i-3] : dpSe[i-2];
+        } else if (i >= 2) {
+            dpSe[i] += dpSe[i-2];
+        }
+        if (max < dpSe[i]) max = dpSe[i];
+    }
+    
+    return max;
+}
