875fce9d698fb4a50d779bf4d459ab50718addc7
znznakd <64431656+znznakd@users.noreply.github.com>
2025-02-28 23:05:55 +0900

    Update 10Algorithm.java

--- study/10Algorithm.java
+++ study/10Algorithm.java
@@ -1,1 +1,29 @@
-test
+public double interestRate(double price, double monthlyPayment, int loanTerm) {
+    
+    double balance;
+    double ret = 50;
+    double top = 100;
+    double low = 0;
+    
+    while(true) {
+        
+        balance = price;
+        
+        for (int i = 0; i< loanTerm; i++) {
+            balance *= ret/1200 + 1;
+            balance -= monthlyPayment;
+        }
+        
+        if (balance < 1e-9 && balance > -(1e-9)) {
+            break;
+        } else if (balance > 0) {
+            top = ret;
+            ret -= (ret-low)/2;
+        } else {
+            low = ret;
+            ret += (top-ret)/2;
+        }
+        
+    }
+    
+    return ret; 
