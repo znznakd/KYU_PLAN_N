612b4b0c78abc8d2c78948c269ec6ab9e6d6d6d6
znznakd <64431656+znznakd@users.noreply.github.com>
2025-02-28 23:07:53 +0900

    Update 14Algorithm.java

--- study/14Algorithm.java
+++ study/14Algorithm.java
@@ -1,1 +1,27 @@
-test
+Map<String, String> map = new HashMap<>();
+
+map.put("K3", "V3");
+map.put("K1", "V1");
+map.put("K2", "V2");
+
+// Case 1
+Iterator<String> keys = map.keySet().iterator();
+while (keys.hasNext()) {
+    String key = keys.next();
+    System.out.println(key + " " + map.get(key));
+}
+
+// Case 2
+for (Map.Entry<String, String> elem : map.entrySet()) {
+    System.out.println(elem.getKey() + " " + elem.getValue());
+}
+
+// Case 3
+for (String key : map.keySet()) {
+    System.out.println(key + " " + map.get(key));
+}
+
+// Output - 3가지 방법 다 동일
+K2 V2
+K1 V1
+K3 V3
