d465055ff6863acd415981e2534eeec87e29c5bc
znznakd <64431656+znznakd@users.noreply.github.com>
2025-02-28 23:07:06 +0900

    Update 12Algorithm.java

--- study/12Algorithm.java
+++ study/12Algorithm.java
@@ -1,1 +1,25 @@
-test
+def dijkstra(graph, start):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    visited = set()
+
+    while visited != set(graph.keys()):
+        current_vertex = min((vertex for vertex in distances if vertex not in visited), key=lambda vertex: distances[vertex])
+        
+        for neighbor, weight in graph[current_vertex].items():
+            new_distance = distances[current_vertex] + weight
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+        # 현재 노드를 방문한 것으로 표시합니다.
+        visited.add(current_vertex)
+    
+    return distances
+
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+print(dijkstra(graph, 'A'))
